e6ec98b5795b64f2e09db8366d2fa369b648f38f
Kim Jonghoon <kimchonghun@naver.com>
2020-07-21 21:25:42 +0900

    tiles.xml updated

--- src/main/java/net/java_school/commons/WebContants.java
+++ src/main/java/net/java_school/commons/WebContants.java
@@ -2,6 +2,6 @@ package net.java_school.commons;
 
 public class WebContants {
 
-	public final static String GO_TO_PAGE_AFTER_LOGIN = "/bbs/chat?page=1";
+	public final static String GO_TO_PAGE_AFTER_LOGIN = "/blog";
 
 }
--- src/main/java/net/java_school/controller/BlogController.java
+++ src/main/java/net/java_school/controller/BlogController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.googlecode.objectify.Key;
 
@@ -18,7 +17,6 @@ import net.java_school.blog.Category;
 import net.java_school.blog.Lang;
 
 @Controller
-@RequestMapping("blog")
 public class BlogController {
 
 	private String getChapter(String category) {
@@ -49,15 +47,75 @@ public class BlogController {
 			case "google-app-engine":
 				chapter = "Google Cloud";
 				break;	
-			case "blog":
-				chapter = "Blog";
-				break;
 		}
 
 		return chapter;
 	}
-	
-	@GetMapping
+
+	@GetMapping("{category:(?:java)|(?:jdbc)|(?:jsp)|(?:css-layout)|(?:jsp-pjt)|(?:spring)|(?:javascript)|(?:google-app-engine)}")
+	public String getFrstBlog(Model model, Locale locale, @PathVariable String category) {
+		String lang = locale.getLanguage();
+		Key<Lang> theLang = Key.create(Lang.class, lang);
+		Key<Category> theCategory = Key.create(theLang, Category.class, category);
+
+		List<Article> articles = ofy()
+			.load()
+			.type(Article.class)
+			.ancestor(theLang)
+			.ancestor(theCategory)
+			.order("date")
+			.list();
+
+		model.addAttribute("articles", articles);
+
+		Article article = ofy()
+			.load()
+			.type(Article.class)
+			.ancestor(theLang)
+			.ancestor(theCategory)
+			.order("date")
+			.first()
+			.now();
+
+		if (article == null) return "redirect:/";
+		model.addAttribute("article", article);
+
+		String chapter = this.getChapter(category);
+		model.addAttribute("chapter", chapter);
+
+		return "tutorial/" + category;
+
+	}
+
+	@GetMapping("{category:(?:java)|(?:jdbc)|(?:jsp)|(?:css-layout)|(?:jsp-pjt)|(?:spring)|(?:javascript)|(?:google-app-engine)}/{id}")
+	public String getBlog(Model model, Locale locale, @PathVariable String category, @PathVariable String id) {
+		String lang = locale.getLanguage();
+		Key<Lang> theLang = Key.create(Lang.class, lang);
+		Key<Category> theCategory = Key.create(theLang, Category.class, category);
+
+		Key<Article> theArticle = Key.create(theCategory, Article.class, id);
+		Article article = ofy().load().key(theArticle).now();
+		if (article == null) return "redirect:/";
+
+		model.addAttribute("article", article);
+
+		List<Article> articles = ofy()
+			.load()
+			.type(Article.class)
+			.ancestor(theLang)
+			.ancestor(theCategory)
+			.order("date")
+			.list();
+
+		model.addAttribute("articles", articles);
+
+		String chapter = this.getChapter(category);
+		model.addAttribute("chapter", chapter);
+
+		return "tutorial/" + category;
+	}
+
+	@GetMapping("blog")
 	public String index(Model model, Locale locale) {
 		String lang = locale.getLanguage();
 		Key<Lang> theLang = Key.create(Lang.class, lang);
@@ -76,7 +134,7 @@ public class BlogController {
 		return "blog";
 	}
 
-	@GetMapping("{id}")
+	@GetMapping("blog/{id}")
 	public String blog(@PathVariable("id") String id, Locale locale, Model model) {
 		String lang = locale.getLanguage();
 		Key<Lang> theLang = Key.create(Lang.class, lang);
@@ -88,5 +146,4 @@ public class BlogController {
 
 		return "blog/" + id;
 	}
-
 }
